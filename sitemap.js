8419d1003042598afd149e9d1fe7183e371206b9
danbernardi <danbernardi1@gmail.com>
2021-09-16 15:21:13 -0700

    Updates sitemap urls

--- sitemap.js
+++ sitemap.js
@@ -4,7 +4,7 @@ import Generator from 'react-router-sitemap-generator';
 import Router from './src/Routes';
 
 const generator = new Generator(
-  'https://pa11y-ci-demo.netlify.app/',
+  'https://pa11y-ci-demo.netlify.app',
   Router(),
   {
     lastmod: new Date().toISOString().slice(0, 10),
